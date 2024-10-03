aca19fc89909a0c34323b135b56e46aad7403e24
Umar_Aliy <umaraliy092@gmail.com>
2024-10-03 23:27:51 +0900

    feat: developed agent detail page logic

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -79,6 +79,7 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 
   useEffect(() => {
     console.log("searchFilter", searchFilter);
+    getPropertiesRefetch({ input: searchFilter });
   }, [searchFilter]);
 
   /** HANDLERS **/
